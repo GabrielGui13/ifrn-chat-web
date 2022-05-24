adc9a9862562a796e5e5179c918b59c57f7e1d7e
Leonardo Minora <leonardo.minora@gmail.com>
2022-05-23 21:28:43 -0300

    Created internal components

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
+const AppContainer = props => <div>{props.children}</div>;
+const AppInput = props => (
+  <div>
+    <label>{props.label}</label>
+    <input type={props.type} title={props.title} />
+  </div>
+);
+const AppButton = props => (
+  <div>
+    <input type="submit" title={props.title} />
+  </div>
+);
+
 const Login = props => {
   return (
-    <div>
-      <div>
-        <label>Usuário: </label>
-        <input type="text" title="apelido ou email do usuário" />
-      </div>
-      <div>
-        <label>Senha: </label>
-        <input type="password" title="senha do usuário" />
-      </div>
-      <div>
-        <input type="submit" title="Entrar" />
-      </div>
-    </div>
+    <AppContainer>
+      <AppInput
+        title="apelido ou email do usuário"
+        label="Usuário:"
+        type="text"
+      />
+      <AppInput label="Senha:" type="password" title="senha do usuário" />
+      <AppButton title="Entrar" />
+    </AppContainer>
   );
 };
 
